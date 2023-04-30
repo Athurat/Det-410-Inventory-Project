3be0c49ff14ae02cb7f8abbe85ceed942f33d9a9
Athurat <122384429+Athurat@users.noreply.github.com>
2023-04-29 21:28:39 -0500

    4/29/2023 (Having button issues)

--- DET 410 GUI Adding Window.py
+++ DET 410 GUI Adding Window.py
@@ -39,21 +39,21 @@ class HeadGUI:
         entry = tk.Entry(self.middleframe, width=20, font=('Arial', 40))
         entry.pack(expand=True, fill='y')
 
+        #Select Button
+        Selectbutton = tk.Button(self.lowerframe, text='SELECT', width=30, height=5)
+        Selectbutton.pack(side=tk.LEFT)
+
         #Create Add button
-        Addbutton = tk.Button(self.lowerframe, text='ADD', command='search', width=30, height=5)
+        Addbutton = tk.Button(self.lowerframe, text='ADD', width=30, height=5)
         Addbutton.pack(side=tk.LEFT)
 
         #Create Delete Button
-        Deletebutton = tk.Button(self.lowerframe, text='DELETE', command='search', width=30, height=5)
+        Deletebutton = tk.Button(self.lowerframe, text='DELETE', width=30, height=5)
         Deletebutton.pack(side=tk.LEFT)
 
-        #Create Edit Button
-        Editbutton = tk.Button(self.lowerframe, text='EDIT', command='search', width=30, height=5)
+        #Create Edit/Reserve Button
+        Editbutton = tk.Button(self.lowerframe, text='EDIT/RESERVE', width=30, height=5)
         Editbutton.pack(side=tk.LEFT)
-
-        #Create  Reserve Button
-        Reservebutton = tk.Button(self.lowerframe, text='RESERVE', command='search', width=30, height=5)
-        Reservebutton.pack(side=tk.LEFT)
 
         #Create a row of stuff for editing
         id = Label(self.editframe, text="Category",relief="ridge")
@@ -146,61 +146,55 @@ class HeadGUI:
         Inventorylist.insert('', 'end', values=('Row 5 Column 1', 'Row 5 Column 2', 'Row 5 Column 5'))
         Inventorylist.insert('', 'end', values=('Row 6 Column 1', 'Row 6 Column 2', 'Row 6 Column 6'))
 
+    # Select Row and Edit Row
+    def Add_button(self):
+        # clear entry boxes
+        id_entry.delete(0, END)
+        Category_entry.delete(0, END)
+        itemdesc_entry.delete(0, END)
+        location_entry.delete(0, END)
+        purchaselocation_entry.delete(0, END)
+        quality_entry.delete(0, END)
+        quantity_entry.delete(0, END)
+        reservation_entry.delete(0, END)
 
-        #Select Row and Edit Row
-        def select_record():
-            # clear entry boxes
-            id_entry.delete(0, END)
-            Category_entry.delete(0, END)
-            itemdesc_entry.delete(0, END)
-            location_entry.delete(0, END)
-            purchaselocation_entry.delete(0, END)
-            quality_entry.delete(0,END)
-            quantity_entry.delete(0,END)
-            reservation_entry.delete(0,END)
+        # Get row that has focus
+        selected = Inventorylist.focus()
+        # grab record values
+        values = Inventorylist.item(selected, 'values')
 
-            # Get row that has focus
-            selected = Inventorylist.focus()
-            # grab record values
-            values = Inventorylist.item(selected, 'values')
+        # Insert focus row in entry boxes
+        id_entry.insert(0, values[0])
+        Category_entry.insert(0, values[0])
+        itemdesc_entry.insert(0, values[0])
+        location_entry.insert(0, values[0])
+        purchaselocation_entry.insert(0, values[0])
+        quality_entry.insert(0, values[0])
+        quantity_entry.insert(0, values[0])
+        reservation_entry.insert(0, values[0])
 
-            # Insert focus row in entry boxes
-            id_entry.insert(0, values[0])
-            Category_entry.insert(0, values[0])
-            itemdesc_entry.insert(0, values[0])
-            location_entry.insert(0, values[0])
-            purchaselocation_entry.insert(0, values[0])
-            quality_entry.insert(0, values[0])
-            quantity_entry.insert(0, values[0])
-            reservation_entry.insert(0, values[0])
+    # Update Record
+    def update_record(self):
+        selected = Inventorylist.focus()
 
-        # Update Record
-        def update_record():
-            selected = Inventorylist.focus()
+        # save new data
+        Inventorylist.item(selected, text="",
+                           values=(id_entry.get(), Category_entry.get(), itemdesc_entry.get(), location_entry.get(),
+                                   purchaselocation_entry.get(), quality_entry.get(), quantity_entry.delete.get(),
+                                   reservation_entry.get()))
 
-            # save new data
-            Inventorylist.item(selected, text="",
-                         values=(id_entry.get(), Category_entry.get(), itemdesc_entry.get(), location_entry.get(), purchaselocation_entry.get(),quality_entry.get(), quantity_entry.delete.get(),reservation_entry.get()))
-
-            # clear entry boxes
-            id_entry.delete(0, END)
-            Category_entry.delete(0, END)
-            itemdesc_entry.delete(0, END)
-            location_entry.delete(0, END)
-            purchaselocation_entry.delete(0, END)
-            quality_entry.delete(0, END)
-            quantity_entry.delete(0, END)
-            reservation_entry.delete(0, END)
-
-
-
-
-
-
+        # clear entry boxes
+        id_entry.delete(0, END)
+        Category_entry.delete(0, END)
+        itemdesc_entry.delete(0, END)
+        location_entry.delete(0, END)
+        purchaselocation_entry.delete(0, END)
+        quality_entry.delete(0, END)
+        quantity_entry.delete(0, END)
+        reservation_entry.delete(0, END)
 
         # Run the GUI main loop
         self.root.mainloop()
-
 
 if __name__ == '__main__':
     mainGUI = HeadGUI()
